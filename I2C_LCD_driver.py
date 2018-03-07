8ec9f9518a765cd4142415e3f24c864fc45a3631
vivienkit <kit038@gmail.com>
2018-03-07 16:44:32 +0100

    ajout : récupération des arguments nobre de boots effectués / manqués (relatifs à la reboot machine du repo d'à côté)

--- I2C_LCD_driver.py
+++ I2C_LCD_driver.py
@@ -176,5 +176,4 @@ class lcd:
       self.lcd_write(0x40);
       for char in fontdata:
          for line in char:
-            self.lcd_write_char(line)         
-         
+            self.lcd_write_char(line)
--- write2lcd.py
+++ write2lcd.py
@@ -1,9 +1,13 @@
-#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
 import I2C_LCD_driver
+import os, sys
 from time import *
 
-mylcd = I2C_LCD_driver.lcd()
+lcd = I2C_LCD_driver.lcd()
 
-mylcd.lcd_display_string("Hello World!", 1, 2)
-mylcd.lcd_display_string("Hello World!", 2, 2)
+nbre_boot = sys.argv[1]
+manques = sys.argv[2]
+#print(nbre_boot, manques)
+lcd.lcd_display_string("boot number:" + nbre_boot , 1)
+lcd.lcd_display_string("Failed:" + manques, 2)
