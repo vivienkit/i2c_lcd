745de14d54339ee0c269854aaf69712863a4550c
vivienkit <kit038@gmail.com>
2018-03-07 14:44:40 +0100

    initial commit

---
+++ write2lcd.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+
+import I2C_LCD_driver
+from time import *
+
+mylcd = I2C_LCD_driver.lcd()
+
+mylcd.lcd_display_string("Hello World!", 1, 2)
+mylcd.lcd_display_string("Hello World!", 2, 2)
